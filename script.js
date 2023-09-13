58ce8e644bf3770d8bcedee814d4dee7a3fd77ac
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-13 22:29:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+
+const link = document.querySelector('a');
+
+// Function to highlight bold words in green
 function highlight() {
-    //Write your code here
+    // Get all elements with the <strong> tag
+    const boldElements = document.querySelectorAll('strong');
 
-
+    // Change the color of bold elements to green
+    boldElements.forEach(element => {
+        element.style.color = 'green';
+    });
 }
 
-
+// Function to revert the color of bold words to black
 function return_normal() {
-    //Write your code here
+    // Get all elements with the <strong> tag
+    const boldElements = document.querySelectorAll('strong');
 
-    
+    // Revert the color of bold elements to black
+    boldElements.forEach(element => {
+        element.style.color = 'black';
+    });
 }
+
+// Add event listeners to the link for mouseover and mouseout events
+link.addEventListener('mouseover', highlight);
+link.addEventListener('mouseout', return_normal);
